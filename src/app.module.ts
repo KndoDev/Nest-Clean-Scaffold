8590e3d2d249b21be80fb397a8807daf9c82fe17
omar <omar.amell@tiendasisimo.com>
2024-07-17 14:43:00 -0500

    feat(base): 🔧 Add TypeOrm Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configLoader } from '@src/config/config-loader';
 import { envSchema } from '@src/config/env-schema';
+import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from '@src/config/database-config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configLoader],
       validationSchema: envSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
